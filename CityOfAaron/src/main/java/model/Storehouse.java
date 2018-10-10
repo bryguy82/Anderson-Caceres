c9e2a5145b51ee9eed95647a7e26a5f4349db93b
Anderson-Bryan <43304311+bryguy82@users.noreply.github.com>
2018-10-10 20:40:51 -0300

    Update Storehouse.java

--- CityOfAaron/src/main/java/model/Storehouse.java
+++ CityOfAaron/src/main/java/model/Storehouse.java
@@ -10,14 +10,48 @@ package model;
  * @author tonyc
  */
 public class Storehouse {
+
+    private Author[] authors;
+    private Animal[] animals;
+    private InventoryItem[] tools;
+    private Provision[] provisions;
     
     public Storehouse(){
-        //Empty Constructor
+        //Empty constructor
+    }
+    
+    public Author[] getAuthors(){
+        return authors;
+    }
+    public void setAuthors(Author[] authors){
+        this.authors = authors;
+    }
+    public Animal[] getAnimals(){
+        return animals;
+    }
+    public void setAnimals(Animal[] animals){
+        this.animals = animals;
+    }
+    public InventoryItem[] getTools(){
+        return tools;
+    }
+    public void setTools(InventoryItem[] InventoryItem){
+        this.tools = tools;
+    }
+    public Provision[] getProvisions(){
+        return provisions;
+    }
+    public void setProvisions(Provision[] provisions){
+        this.provisions = provisions;
     }
     
     @Override
     public String toString(){
-        return "Storehouse Class coming soon";
+        return "Currently in the Storehouse you have: {"
+                + " these authors: " + authors
+                + ", these animals: " + animals
+                + ", these tools: " + tools
+                + ", and these provisions: " + provisions
+                + "}";
     }
-    
 }
