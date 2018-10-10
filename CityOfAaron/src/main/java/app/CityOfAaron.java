9e2536ef2e6c6e20d148ebf9466f40cfe210b6b3
Angel Caceres <cac18001@byui.edu>
2018-10-10 01:29:31 -0300

    08/10/2018-FirstStep

---
+++ CityOfAaron/src/main/java/app/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+ * Main class. This is the first code that will be executed when 
+ * the program starts.
+ */
+
+package app;
+
+public class CityOfAaron {
+
+    /**
+     * The starting point for our application. 
+     * 
+     * @param args Any parameters passed on the command line will be 
+     * contained in this array.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("It works!");
+
+    }
+
+}
---
+++ CityOfAaron/src/test/java/BasicTest.java
@@ -0,0 +1,14 @@
+// This file provides a simple starter file for JUnit  
+// tests.
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BasicTest {
+
+    @Test
+    public void SimpleTest(){
+        assertTrue(true);
+    }
+
+}
