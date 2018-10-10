c10cd21ada398bf7e16da55b7882cd760155d5e2
Anderson-Bryan <43304311+bryguy82@users.noreply.github.com>
2018-10-10 20:44:55 -0300

    Added more classes
    I started these classes.

---
+++ CityOfAaron/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Animal {
+    
+    @Override
+    public String toString(){
+        return "work in progress";
+    }
+}
