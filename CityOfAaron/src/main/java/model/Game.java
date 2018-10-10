c48342ad31bbe8062b61c4b1a50b2fa3371f3995
Angel Caceres <cac18001@byui.edu>
2018-10-10 01:41:02 -0300

    08/10/2018-FirstStep

---
+++ CityOfAaron/src/main/java/model/Game.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author tonyc
+ */
+public class Game {
+    private Player thePlayer;
+    private Map theMap;
+    private Storehouse theStorehouse;
+    private int currentPopulation;
+    private int acresOwned;
+    private int wheatInStorage;
+}
