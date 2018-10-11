0b0928cb88e61fb6c4c72cc46eb8b95ee0005313
Angel Caceres <cac18001@byui.edu>
2018-10-10 23:03:37 -0300

    some more changes

--- CityOfAaron/src/main/java/app/CityOfAaron.java
+++ CityOfAaron/src/main/java/app/CityOfAaron.java
@@ -6,6 +6,15 @@ import model.Map;
 import model.Player;
 import model.Storehouse;
 
+import model.Author;
+import model.Animal;
+import model.Condition;
+import model.InventoryItem;
+import model.ItemType;
+import model.Location;
+import model.Point;
+import model.Provision;
+
 public class CityOfAaron {
 
     /**
@@ -23,6 +32,7 @@ public class CityOfAaron {
         theGame.setCurrentPopulation(105);
         theGame.setAcresOwned(2000);
         theGame.setWheatInStorage(15000);
+        
         
         System.out.println(theGame.toString());
 
