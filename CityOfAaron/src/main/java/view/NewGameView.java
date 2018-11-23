0309f2cda2cba54883b3e7ef3ef23a92f758e068
Anderson-Bryan <43304311+bryguy82@users.noreply.github.com>
2018-11-23 00:16:08 -0200

    more

--- CityOfAaron/src/main/java/view/NewGameView.java
+++ CityOfAaron/src/main/java/view/NewGameView.java
@@ -54,6 +54,7 @@ public class NewGameView extends ViewStarter {
         }
 
         String playerName = inputs[0];
+        
         createAndStartGame(playerName);
         
         return false;
@@ -70,11 +71,6 @@ public class NewGameView extends ViewStarter {
         System.out.println();
         System.out.println("Welcome to the Game, " + CityOfAaron.getCurrentGame().getThePlayer().getName() + "! \n");
         
-        newGameStartMenu();
-    }
-
-    private void newGameStartMenu() {
-        pause(2000);
         View gameMenu = new GameMenuView();
         gameMenu.displayView();
     }
